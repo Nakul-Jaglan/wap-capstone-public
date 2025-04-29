9e5c8d40f0eb250c24ce36fcce0db7ca821a22fc
nakul <jaglan.nakul@gmail.com>
2025-04-30 01:24:19 +0530

    Added responsiveness in Footer and added 1 more social media icon

--- app/components/footer.js
+++ app/components/footer.js
@@ -20,35 +20,42 @@ function Footer() {
                     <a href="https://www.linkedin.com/in/nakuljaglan/" target="_blank" className="social-links-text">
                         <div className='socials-div'>
                             <img src="/linkedin.png" alt="Logo" className="social-image"/>
-                            <div>Linkedin</div>
+                            <div className='text-inside-link'>Linkedin</div>
                         </div>
                     </a>
 
                     <a href="https://github.com/Nakul-Jaglan" target="_blank" className="social-links-text">
                         <div className='socials-div'>
                             <img src="/github.png" alt="Logo" className="social-image"/>
-                            <div>GitHub</div>
+                            <div className='text-inside-link'>GitHub</div>
                         </div>
                     </a>
 
                     <a href="https://www.youtube.com/@NakulNST" target="_blank" className="social-links-text">
                         <div className='socials-div'>
                             <img src="/youtube.png" alt="Logo" className="social-image"/>
-                            <div>YouTube</div>
+                            <div className='text-inside-link'>YouTube</div>
                         </div>
                     </a>
 
                     <a href="https://www.x.com/Nakul_Jaglan" target="_blank" className="social-links-text">
                         <div className='socials-div'>
                             <img src="/twitter.png" alt="Logo" className="social-image"/>
-                            <div>Twitter</div>
+                            <div className='text-inside-link'>Twitter</div>
                         </div>
                     </a>
 
                     <a href="https://www.instagram.com/nakul.jaglan" target="_blank" className="social-links-text">
                         <div className='socials-div'>
                             <img src="/instagram.png" alt="Logo" className="social-image"/>
-                            <div>Instagram</div>
+                            <div className='text-inside-link'>Instagram</div>
+                        </div>
+                    </a>
+                    
+                    <a href='https://www.reddit.com/r/newtonschooloftech/' target="_blank" className="social-links-text">
+                        <div className='socials-div'>
+                            <img src="/reddit.png" alt="Logo" className="social-image"/>
+                            <div className='text-inside-link'>Reddit</div>
                         </div>
                     </a>
                     
@@ -59,4 +66,4 @@ function Footer() {
   )
 }
 
-export default Footer
+export default Footer 
